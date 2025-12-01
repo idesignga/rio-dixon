c1a8e94edf7f155d04e9b11c41f35e84aef22e50
brianpena <brian@idesignga.com>
2025-12-01 00:01:25 -0500

    Initial Commit. Website for Rio Dixon mental health coach.

---
+++ src/assets/js/about.js
@@ -0,0 +1,39 @@
+function togglePlayButton() {
+    // Select all elements with the .cs-picture class
+    const pictures = document.querySelectorAll('#quote-1889 .cs-video-wrapper');
+  
+    // Add a click event listener to each .cs-picture element
+    pictures.forEach(picture => {
+      picture.addEventListener('click', () => {
+        // Select all elements with the .cs-play class
+        const playButtons = document.querySelectorAll('#quote-1889 .cs-play');
+        
+        // Toggle the .cs-hide class on each .cs-play element
+        playButtons.forEach(button => {
+          button.classList.toggle('cs-hide');
+        });
+      });
+    });
+  }
+  
+  // Call the function to activate the event listeners
+  togglePlayButton();
+  
+  function toggleVideoPlayback() {
+    // Select the video element
+    const video = document.querySelector('#quote-1889 video');
+  
+    // Add a click event listener to the video
+    video.addEventListener('click', () => {
+      // Check if the video is paused
+      if (video.paused) {
+        video.play(); // Play the video if it is paused
+      } else {
+        video.pause(); // Pause the video if it is playing
+      }
+    });
+  }
+  
+  // Call the function to activate the event listener
+  toggleVideoPlayback();
+                                
